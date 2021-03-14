ef77b0bdeacda0a8427e261c2a8db026bbcf1d64
Vcar121 <2234265899@qq.com>
2021-03-14 12:34:36 +0800

    错误处理

---
+++ src/views/3-5.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="class"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  name: "axios3-4",
+  created() {
+    //错误处理
+    axios.interceptors.request.use(
+      (config) => {
+        return config;
+      },
+      (err) => {
+        return err;
+      }
+    );
+    axios.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        return err;
+      }
+    );
+    axios
+      .get("")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //实际开发
+    let instance = axios.create();
+    instance.interceptors.request.use(
+      (config) => {
+        return config;
+      },
+      (err) => {
+        return err;
+      }
+    );
+    instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        return err;
+      }
+    );
+    instance.get("").then();
+  },
+};
+</script>
